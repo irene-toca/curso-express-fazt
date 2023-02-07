8b23d43a37e0228211e1d6e0b178c8b4f560d36f
Irene Toca <irene.devops@gmail.com>
2023-02-07 21:17:34 +0000

    http response

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-// const http = require ('http')
-// const fs = require('fs')
-
-// const server = http.createServer((req,res)=> {
-//     const read = fs.createReadStream('./static/index.html')
-//     read.pipe(res)
-
-// }) 
-
-// server.listen(3000)
-// console.log(`Server on port ${3000}`)
-
-
-const express = require('express')
+const express = require ('express')
 
 const app = express()
 
-app.get('/', (req,res)=> {
+app.get('/',(req,res)=>{
     res.send('Hello World')
-    
+
 })
 
-app.get('/about', (req,res)=> {
-    res.send('About')
-    
+app.get('/',(req,res)=>{
+    res.send('Hello World')
 })
-app.get('/weather', (req,res)=> {
-    res.send('The current weather is Nice')
-    
-})
-
-app.use((req,res)=> {
-    res.status(404).send('No se encontro la pagina')
-})
-    
-
 
 
 app.listen(3000)
