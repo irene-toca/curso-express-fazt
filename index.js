92f5e0547cfbfc16aadb5682406f1947ac1bf0e1
Irene Toca <irene.devops@gmail.com>
2023-02-16 23:20:08 +0000

    http response

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require ('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-})
+app.get("/miarchivo", (req, res) => {
+  res.sendFile("./imagen.jpg", {
+    root: __dirname,
+  });
+});
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+app.get("/user", (req, res) => {
+  res.json({
+    naem: "fazt",
+    lastname: "ray",
+    age: 40,
+    points: [1, 2, 3],
+    address: {
+      city: "new york",
+      street: "sone street 123",
+    },
+  });
+});
 
+app.get("/isAlive", (req, res) => {
+    res.sendStatus(204);
+  });
 
-app.listen(3000)
-console.log(`Server on port ${3000}`)
+app.listen(3000);
+console.log(`Server on port ${3000}`);
