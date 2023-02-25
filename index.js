7dddc066e9de7a4c758c3a59eba5f49a17315531
Irene Toca <irene.devops@gmail.com>
2023-02-25 21:44:12 +0000

    request params

--- index.js
+++ index.js
@@ -2,32 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./imagen.jpg", {
-    root: __dirname,
-  });
-});
+app.post('/user', (req,res) =>{
+  console.log(req.body)
+  res.send('Nuevo usuario creado')
 
-app.get("/user", (req, res) => {
-  res.json({
-    naem: "fazt",
-    lastname: "ray",
-    age: 40,
-    points: [1, 2, 3],
-    address: {
-      city: "new york",
-      street: "sone street 123",
-    },
-  });
-});
+})
 
-app.get("/isAlive", (req, res) => {
-    res.sendStatus(204);
-  });
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
