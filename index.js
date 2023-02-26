ca6e0a7905472eedba3d03415948ed486af5a6b8
Irene Toca <irene.devops@gmail.com>
2023-02-26 21:23:11 +0000

    request params

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.get('/hello/:username', (req, res)=>{
+  console.log(typeof req.params.username)
+  res.send(`Hello ${req.params.username.toUpperCase()}`)
+})
+app.get('/add/:x/:y',(req,res)=> {
+  
+  const {x,y}= req.params
+  
+  res.send(`Result: ${parseInt(x) + parseInt(y)}`)
+})
 
-app.post('/user', (req,res) =>{
-  console.log(req.body)
-  res.send('Nuevo usuario creado')
+app.get('/user/:username/photo',(req,res)=>{
+  if (req.params.username === "fazt"){
+    return res.sendFile('imagen.jpg',{
+      root: __dirname
+    })
+      
+  }
+  res.send('el usuario no tiene acceso')
 
 })
+app.get('/name/:nombre/age/:age',(req,res)=>{
+  console.log(req.params)
+  res.send(`El usuario ${req.params.nombre} tiene ${req.params.age} años`)
+})
+
 
 
 app.listen(3000);
