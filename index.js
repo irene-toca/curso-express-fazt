7210710f4c9e7b7ba6ba75bbd6b53f792c89d7cc
Irene Toca <irene.devops@gmail.com>
2023-03-03 18:38:34 +0000

    middlewares

--- index.js
+++ index.js
@@ -2,43 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.all('/info',(req,res)=>{
-  res.send('server info')
+app.use((req, res,next)=> {
+
+    console.log(`Route:${req.url} Metodo:${req.method}`)
+
+    next()
 })
 
-app.get('/search', (req,res )=> {
-  console.log(req.query)
-  if(req.query.q ==='javascript books') {
-    res.send('lista de libros de javascript')
-  } else{
-    res.send('pagina normal')
-  }
 
+app.get('/profile', (req, res)=>{
+    res.send('profile page')
 })
 
-app.get('/hello/:username', (req, res)=>{
-  res.send(`Hello ${req.params.username.toUpperCase()}`)
-})
-app.get('/add/:x/:y',(req,res)=> {
-  
-  const {x,y}= req.params
-  
-  res.send(`Result: ${parseInt(x) + parseInt(y)}`)
-})
 
-app.get('/user/:username/photo',(req,res)=>{
-  if (req.params.username === "fazt"){
-    return res.sendFile('imagen.jpg',{
-      root: __dirname
-    })
-      
-  }
-  res.send('el usuario no tiene acceso')
 
-})
-app.get('/name/:nombre/age/:age',(req,res)=>{
-  console.log(req.params)
-  res.send(`El usuario ${req.params.nombre} tiene ${req.params.age} años`)
+app.all('/about', (req, res)=>{
+    res.send('about page')
 })
 
 app.listen(3000);
