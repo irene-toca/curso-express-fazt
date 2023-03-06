ea19450fee09778d379e31339b77f64bc4c7fc00
Irene Toca <irene.devops@gmail.com>
2023-03-06 10:52:34 +0000

    intoduccion a middlewares

--- index.js
+++ index.js
@@ -20,5 +20,30 @@ app.all('/about', (req, res)=>{
     res.send('about page')
 })
 
+
+
+app.use((req, res,next)=> {
+    
+    if(req.query.login === 'fazt@faztweb.com'){
+
+    next()
+
+    }else{
+
+        res.send('No autorizado')
+
+    }
+
+})
+
+app.get('/dashboard', (req,res)=>{
+
+    res.send('Dashboard page')
+
+})
+
+
+
+
 app.listen(3000);
 console.log(`Server on port ${3000}`);
